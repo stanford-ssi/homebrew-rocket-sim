fb43415bed90a34670f9b4a1a98c17a526ce27b8
Rayan <rayansud@stanford.edu>
2019-07-31 11:21:18 +0200

    Fixed folder structure again
    Sorry its messy af right now

--- RocketSim/main.py
+++ RocketSim/main.py
@@ -6,21 +6,24 @@ from NRLMSISE00.nrlmsise_00 import *
 from DigitalDATCOM.datcom_lookup import lookup
 import xml.etree.ElementTree as ET
 import quaternion
+from scipy.interpolate import griddata
+
 #All SI units
 rocket_mass = 16 #not including motor
 time_step = 3
 c_d = 0.5
 area = 0.00258064
-
-
 altitude = 0
 g= 9.8
 time = 0
 velocity = 0
+acc=0
 positions = []
 velocities = []
 accelerations = []
 times = []
+
+
 
 def parse_thrustcurve():
     tree = ET.parse('Cesaroni_N5800.xml')
@@ -54,52 +57,65 @@ def get_d_t_for_altitude(altitude):
     gtd7(Input, flags, output)
     return output.d[5]*1000, output.t[1] #total air density in KG/M^3
 
-def main():
-    """This is the main function"""
-    motor_masses, motor_thrusts, sample_times = parse_thrustcurve()
-    while True:
-        time += time_step
+'''def loadDATCOM():
+    f = readPickle()
 
-        thrust=0
-        mass = rocket_mass
+def coeff_for_conditions(mach, alpha, altitude, cg, mass)
 
+    loadDATCOM()
+
+    return CD,CL,CM,CN,CA,XCP,CLA,CMA,CYB,CNB,CLB'''
+
+motor_masses, motor_thrusts, sample_times = parse_thrustcurve()
+#loadDATCOM()
+while True:
+    times.append(time)
+    positions.append(altitude)
+    velocities.append(velocity)
+    accelerations.append(acc)
+    time += time_step
+
+    thrust=0
+    mass = rocket_mass
+
+    idx = -1
+    try:
+        idx = np.where(np.isclose(sample_times,time))[0][0]
+    except IndexError:
         idx = -1
-        try:
-            idx = np.where(np.isclose(sample_times,time))[0][0]
-        except IndexError:
-            idx = -1
 
-        if idx!=-1:
-            mass += motor_masses[idx]
-            thrust += motor_thrusts[idx]
+    if idx!=-1:
+        mass += motor_masses[idx]
+        thrust += motor_thrusts[idx]
 
-        weight = mass*g
+    weight = mass*g
 
-        density, temperature = get_d_t_for_altitude(altitude)
-        mach = velocity/(20.05*np.sqrt(temperature))
+    density, temperature = get_d_t_for_altitude(altitude)
+    mach = velocity/(20.05*np.sqrt(temperature))
 
-        mach=0.4
-        alpha=1.0
-        cg=1.2
+    mach=0.4
+    alpha=1.0
+    cg=1.2
 
-        CD,CL,CM,CN,CA,XCP,CLA,CMA,CYB,CNB,CLB = lookup(mach, alpha, altitude, cg, mass)
+    '''CD,CL,CM,CN,CA,XCP,CLA,CMA,CYB,CNB,CLB = coeff_for_conditions(mach, alpha, altitude, cg, mass)'''
+    CD = 0.5
+    drag_force = 0.5*density*(velocity**2)*area*CD
 
-        drag_force = 0.5*density*(velocity**2)*area*CD
+    net_force = thrust-weight-drag_force
+    acc = net_force/mass
+    delta_v = acc*time_step
+    velocity += delta_v
+    delta_x = velocity*time_step
+    altitude += delta_x
 
-        net_force = thrust-weight-drag_force
-        acc = net_force/mass
-        delta_v = acc*time_step
-        velocity += delta_v
-        delta_x = velocity*time_step
-        altitude += delta_x
+    print altitude
 
-        print altitude
 
+    if altitude<0:
         times.append(time)
         positions.append(altitude)
         velocities.append(velocity)
         accelerations.append(acc)
-        if altitude<-80:
-            break
-    plt.plot(times,positions)
-    plt.show()
+        break
+plt.plot(times,positions)
+plt.show()
